79dbb81b249192fefcffb0e87fa1178b366a7889
antiduhring <83927931+antiduhring@users.noreply.github.com>
2023-12-20 05:37:57 +0100

    feature/spring-data-mongo (#3)
    * Spring data mongo support
    
    * Migrate data layer to spring mongo
    
    * Add clear method

---
+++ src/main/kotlin/org/antiduhring/gallery/item/ItemRepository.kt
@@ -0,0 +1,73 @@
+package org.antiduhring.gallery.item
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.FindAndModifyOptions.options
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.query.Criteria.where
+import org.springframework.data.mongodb.core.query.Query.query
+import org.springframework.data.mongodb.core.query.Update
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Component
+
+const val ITEMS_ID_SEQUENCE = "items_id_sequence"
+
+@Document
+data class Sequence(
+    @Id
+    val id: String? = null,
+    val seq: Long = 0
+)
+@Component
+class ItemIdGenerator(private val mongoOperations: MongoOperations) {
+    fun getNext(): Long {
+        var sequence: Sequence? = mongoOperations.findAndModify(
+            query(where("_id").`is`(ITEMS_ID_SEQUENCE)),
+            Update().inc("seq", 1),
+            options().returnNew(true),
+            Sequence::class.java
+        )
+        if (sequence == null) {
+            sequence = Sequence(ITEMS_ID_SEQUENCE, 0)
+            mongoOperations.insert<Any>(sequence)
+        }
+        return sequence.seq
+    }
+    fun reset() {
+        mongoOperations.findAndModify(
+            query(where("_id").`is`(ITEMS_ID_SEQUENCE)),
+            Update().set("seq", 0),
+            options().returnNew(true),
+            Sequence::class.java)
+    }
+}
+
+interface ItemRepository: MongoRepository<ItemDocument, Int>
+interface ItemFileRepository: MongoRepository<ItemFileDocument, Int>
+
+@Document("item")
+data class ItemDocument (
+    @Id
+    var id: Long,
+    val name: String,
+)
+
+@Document("item_file")
+data class ItemFileDocument (
+    @Id
+    var id: Long,
+    val file: ByteArray,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ItemFileDocument
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
--- src/main/kotlin/org/antiduhring/gallery/item/ItemService.kt
+++ src/main/kotlin/org/antiduhring/gallery/item/ItemService.kt
@@ -1,19 +1,34 @@
 package org.antiduhring.gallery.item
 
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class ItemService(private val itemDao: ItemDao) {
+class ItemService(
+    private val itemIdGenerator: ItemIdGenerator,
+    private val itemRepository: ItemRepository,
+    private val itemFileRepository: ItemFileRepository) {
 
-    fun getItems(): List<Item> = itemDao.getItems()
+    fun getItems(): List<Item> = itemRepository.findAll().map(this::map)
 
     //todo: make transactional
     fun createItem(name: String, file: ByteArray): Item {
-        val item = itemDao.insertItem(name)
-        itemDao.insertFile(item.id, file)
-        return item
+        val id = itemIdGenerator.getNext()
+        val document = ItemDocument(id = id, name = name)
+        val saved = itemRepository.save(document)
+        itemFileRepository.insert(ItemFileDocument(saved.id, file))
+        return map(saved)
     }
-    fun loadImage(id: Int): ByteArray = itemDao.getImage(id)
+    fun loadImage(id: Int): Optional<ByteArray> = itemFileRepository.findById(id).map{it.file}
+
+    fun clearAll() {
+        itemRepository.deleteAll()
+        itemFileRepository.deleteAll()
+        itemIdGenerator.reset()
+    }
+
+    private fun map(document: ItemDocument): Item =
+        Item(id = document.id, name = document.name, imageUrl = "/$IMAGE_URL/${document.id}")
 }
 
-data class Item(val id: Int, val name: String, val imageUrl: String)
+data class Item(val id: Long, val name: String, val imageUrl: String)
